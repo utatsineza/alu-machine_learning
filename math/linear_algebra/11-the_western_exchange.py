241ce37c80689ebaf9ffec909b45a8cea8cecf18
utatsineza <h.utatsinez@alustudent.com>
2025-09-14 21:17:44 +0000

    linear algebra

--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-
-def np_transpose(matrix: np.ndarray) -> np.ndarray:
+def np_transpose(matrix):
     """
     Return the transpose of a NumPy array.
 
@@ -9,24 +8,6 @@ def np_transpose(matrix: np.ndarray) -> np.ndarray:
 
     Returns:
         numpy.ndarray: Transposed array.
-            - For 1D arrays, returns as a 2D column vector.
-            - For 2D arrays, returns the usual transpose.
-            - For N-D arrays, reverses the order of axes.
-
-    Examples:
-        >>> np_transpose(np.array([1, 2, 3]))
-        array([[1],
-               [2],
-               [3]])
-
-        >>> np_transpose(np.array([[1, 2], [3, 4]]))
-        array([[1, 3],
-               [2, 4]])
-
-        >>> np_transpose(np.array([[[1, 2]], [[3, 4]]])).shape
-        (2, 1, 2)
     """
-    if matrix.ndim == 1:
-        return matrix.reshape(-1, 1)  # column vector
     return matrix.T
 
