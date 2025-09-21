267bf4e81afc4cdbdebe9998af0106eb709b9308
utatsineza <h.utatsinez@alustudent.com>
2025-09-21 20:36:59 +0000

    advanced linear algebra

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -3,6 +3,7 @@
 Module to calculate the determinant of a square matrix.
 """
 
+
 def determinant(matrix):
     """
     Calculates the determinant of a square matrix.
@@ -18,8 +19,7 @@ def determinant(matrix):
         ValueError: If matrix is not square.
     """
     # Check if matrix is a list of lists
-    if not isinstance(matrix, list) or not all(isinstance(row, list) 
-          for row in matrix):
+    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a list of lists")
 
     # Check if matrix is empty representing 0x0
