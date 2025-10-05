a55fd2f8075c9f3191dc453b66542604d0d25507
utatsineza <h.utatsinez@alustudent.com>
2025-10-05 18:54:50 +0000

    calculus

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -21,7 +21,7 @@ def poly_derivative(poly):
     # Validate input
     if not isinstance(poly, list):
         return None
-    if not all(isinstance(x, (int, float))):
+    if not all(isinstance(x, (int, float))for x in poly):
         return None
 
     # Derivative of constant polynomial
