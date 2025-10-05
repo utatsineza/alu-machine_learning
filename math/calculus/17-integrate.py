c38a3f671ab6115ac6c017e6dc7a78da4a871bac
utatsineza <h.utatsinez@alustudent.com>
2025-10-05 20:30:54 +0000

    calculus

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -12,29 +12,28 @@ def poly_integral(poly, C=0):
     Parameters:
         poly (list of int/float): Coefficients of the polynomial.
             The index represents the power of x.
-        C (int/float): Constant of integration.
+        C (int/float, optional): Constant of integration (default 0).
 
     Returns:
-        list of int/float: Coefficients of the integral.
-            Returns None if input is invalid.
+        list of int/float: Coefficients of the integrated polynomial.
+        Returns None if input is invalid.
     """
-    # Validate input
-    if not isinstance(poly, list) or not all(isinstance(x, (int, float)) for x in poly):
+    if not isinstance(poly, list) or not all(
+        isinstance(x, (int, float)) for x in poly
+    ):
         return None
     if not isinstance(C, (int, float)):
         return None
+    if len(poly) == 0:
+        return None
 
-    # Compute integral coefficients
-    integral = [C]
-    for i in range(len(poly)):
-        coeff = poly[i] / (i + 1)
-        if coeff.is_integer():
-            coeff = int(coeff)
-        integral.append(coeff)
+    result = [C]
+    for i, coeff in enumerate(poly):
+        new_coeff = coeff / (i + 1)
+        if isinstance(new_coeff, float) and new_coeff.is_integer():
+            new_coeff = int(new_coeff)
+        result.append(new_coeff)
 
-    # Remove trailing zeros if not needed
-    while len(integral) > 1 and integral[-1] == 0:
-        integral.pop()
-
-    return integral
-
+    while len(result) > 1 and result[-1] == 0:
+        result.pop()
+    return result
