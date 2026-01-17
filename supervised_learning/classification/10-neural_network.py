91fd63c912409848f047ec80880cbba58a2533a3
kigali kigali <utatsinezahenriette@gmail.com>
2026-01-17 21:13:04 +0200

    neurons-network

--- supervised_learning/classification/10-neural_network.py
+++ supervised_learning/classification/10-neural_network.py
@@ -17,7 +17,6 @@ class NeuralNetwork:
         TypeError: if nx or nodes is not an integer
         ValueError: if nx or nodes is less than 1
         """
-        # Input validation
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
@@ -30,62 +29,56 @@ class NeuralNetwork:
         # Hidden layer
         self.__W1 = np.random.randn(nodes, nx)
         self.__b1 = np.zeros((nodes, 1))
-        self.__A1 = np.zeros((nodes, 1))  # in-place updates possible
+        self.__A1 = 0  # initialized as 0, will store array after forward_prop
 
         # Output neuron
         self.__W2 = np.random.randn(1, nodes)
         self.__b2 = 0
-        self.__A2 = np.zeros((1, 1))      # in-place updates possible
+        self.__A2 = 0  # initialized as 0, will store array after forward_prop
 
-    # --- Getters for private attributes ---
+    # --- Getters ---
     @property
     def W1(self):
-        """Weights for the hidden layer."""
         return self.__W1
 
     @property
     def b1(self):
-        """Bias for the hidden layer."""
         return self.__b1
 
     @property
     def A1(self):
-        """Activated output of hidden layer."""
         return self.__A1
 
     @property
     def W2(self):
-        """Weights for the output neuron."""
         return self.__W2
 
     @property
     def b2(self):
-        """Bias for the output neuron."""
         return self.__b2
 
     @property
     def A2(self):
-        """Activated output of the output neuron."""
         return self.__A2
 
     # --- Forward propagation ---
     def forward_prop(self, X):
         """
-        Perform forward propagation of the neural network.
+        Perform forward propagation.
 
         Parameters:
         X (numpy.ndarray): input data of shape (nx, m)
 
         Updates:
-        __A1 and __A2 in-place
+        __A1 and __A2
 
         Returns:
-        tuple: (__A1, __A2) the same objects as private attributes
+        tuple: (__A1, __A2)
         """
-        # Hidden layer activation (in-place)
-        self.__A1[:] = 1 / (1 + np.exp(-(self.__W1 @ X + self.__b1)))
+        Z1 = self.__W1 @ X + self.__b1
+        self.__A1 = 1 / (1 + np.exp(-Z1))  # sigmoid activation
 
-        # Output layer activation (in-place)
-        self.__A2[:] = 1 / (1 + np.exp(-(self.__W2 @ self.__A1 + self.__b2)))
+        Z2 = self.__W2 @ self.__A1 + self.__b2
+        self.__A2 = 1 / (1 + np.exp(-Z2))  # sigmoid activation
 
         return self.__A1, self.__A2
