d1ebcd71051a3d9e991908a15a0b46fccfb43698
kigali kigali <utatsinezahenriette@gmail.com>
2026-01-17 19:45:25 +0200

    neurons

---
+++ supervised_learning/classification/1-neuron.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import numpy as np
+
+
+class Neuron:
+    """Defines a single neuron performing binary classification"""
+
+    def __init__(self, nx):
+        """
+        nx: number of input features
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be a integer")
+        if nx < 1:
+            raise ValueError("nx must be positive")
+
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """Getter for weights"""
+        return self.__W
+
+    @property
+    def b(self):
+        """Getter for bias"""
+        return self.__b
+
+    @property
+    def A(self):
+        """Getter for activated output"""
+        return self.__A
---
+++ supervised_learning/classification/README.md
@@ -0,0 +1,1 @@
+Neurons task
